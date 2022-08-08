6fbc3067ce5a48647af90e6549bc2dccc9cf6e7f
Casey <casey.oneill@unb.ca>
2022-08-08 10:33:39 -0300

    connecting roomba script to api

--- roomba.py
+++ roomba.py
@@ -1,10 +1,10 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 from pycreate600 import Create
+from pycreate600 import NoConnectionError
 import numpy as np
+import requests
 import random
 import time
-
-from pycreate600.errors import NoConnectionError
 
 
 class Directions(IntEnum):
@@ -14,18 +14,51 @@ class Directions(IntEnum):
     RIGHT = 3
 
 
-directions = [np.array([1, 1]), np.array([-1, -1]), np.array([0, 1]), np.array([1, 0])]
+class State(Enum):
+    NEUTRAL = 0,
+    NEUROTIC = 1
+
+
+directions = [np.array([1, 1]), np.array([-1, -1]),
+              np.array([0, 1]), np.array([1, 0])]
 random.seed()
+
+login_url = "http://localhost:8080/api/auth/login"
+tasks_url = "http://localhost:8080/api/users/1/tasks"
+
+credentials = {
+    "username": "user",
+    "password": "password"
+}
+
+state = State.NEUTRAL
+
+
+def has_task():
+    auth_res = requests.post(login_url, json=credentials).json()
+    auth = "Bearer " + auth_res["jwtToken"]
+
+    tasks = requests.get(tasks_url, headers={"Authorization": auth}).json()
+    if any(i["complete"] == False and i["skipped"] == False for i in tasks):
+        return True
+    return False
+
 
 def clean(bot: Create, vel: int = 200, duration: int = 1):
     """
     Start a custom cleaning cycle.
+
+    Args:
+        bot: Create class controlling Roomba.
+        vel: Roomba movement speed.
+        duration: The length of the cleaning cycle (minutes).
     """
     print("Start cleaning!")
-    timeout = duration * 60
-    is_cleaning = True
-    movement = directions[Directions.FORWARD] * vel
 
+    timeout = duration * 6
+
+    movement = directions[Directions.FORWARD] * vel
+    is_cleaning = True
     bot.motors(13)
 
     while is_cleaning:
@@ -33,8 +66,8 @@ def clean(bot: Create, vel: int = 200, duration: int = 1):
         collision = None
         while is_cleaning and not is_colliding:
             bot.drive_direct(int(movement[0]), int(movement[1]))
-            time.sleep(0.03)
-            timeout -= 0.03
+            time.sleep(5)
+            timeout -= 5
 
             sensors = bot.sensors()
             if sensors.bumps_wheeldrops.bump_left == True and sensors.bumps_wheeldrops.bump_right == True:
@@ -53,38 +86,43 @@ def clean(bot: Create, vel: int = 200, duration: int = 1):
             if timeout <= 0:
                 is_cleaning = False
 
-        bot.drive_stop()
-        time.sleep(0.1)
-        timeout -= 0.1
+            if has_task() == True:
+                state = State.NEUROTIC
 
-        print("Changing directions!")
-        movement = directions[Directions.BACK] * vel
-        bot.drive_direct(int(movement[0]), int(movement[1]))
-        time.sleep(0.5)
-        timeout -= 0.5
+        if is_colliding:
+            bot.drive_stop()
+            time.sleep(0.1)
+            timeout -= 0.1
 
-        if collision == Directions.FORWARD:
-            print("Turning Around.")
-            i = random.randint(0, 1)
-            movement = directions[Directions.LEFT] * vel if i == 0 else directions[Directions.RIGHT] * vel
+            print("Changing directions!")
+            movement = directions[Directions.BACK] * vel
             bot.drive_direct(int(movement[0]), int(movement[1]))
-            time.sleep(1.8)
-            timeout -= 1.8
-        elif collision == Directions.LEFT:
-            print("Turning Right")
-            movement = directions[Directions.RIGHT] * vel
-            bot.drive_direct(int(movement[0]), int(movement[1]))
-            time.sleep(1.8)
-            timeout -= 1.8
-        elif collision == Directions.RIGHT:
-            print("Turning Left!")
-            movement = directions[Directions.LEFT] * vel
-            bot.drive_direct(int(movement[0]), int(movement[1]))
-            time.sleep(1.8)
-            timeout -= 1.8
-        
-        movement = directions[Directions.FORWARD] * vel
-    
+            time.sleep(0.5)
+            timeout -= 0.5
+
+            if collision == Directions.FORWARD:
+                print("Turning Around.")
+                i = random.randint(0, 1)
+                movement = directions[Directions.LEFT] * \
+                    vel if i == 0 else directions[Directions.RIGHT] * vel
+                bot.drive_direct(int(movement[0]), int(movement[1]))
+                time.sleep(1.8)
+                timeout -= 1.8
+            elif collision == Directions.LEFT:
+                print("Turning Right")
+                movement = directions[Directions.RIGHT] * vel
+                bot.drive_direct(int(movement[0]), int(movement[1]))
+                time.sleep(1.8)
+                timeout -= 1.8
+            elif collision == Directions.RIGHT:
+                print("Turning Left!")
+                movement = directions[Directions.LEFT] * vel
+                bot.drive_direct(int(movement[0]), int(movement[1]))
+                time.sleep(1.8)
+                timeout -= 1.8
+
+            movement = directions[Directions.FORWARD] * vel
+
         if timeout <= 0:
             is_cleaning = False
 
